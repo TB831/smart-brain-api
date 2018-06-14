e36a1079e46175468e4689acfab7c88aa22215df
TB831 <druu831@gmail.com>
2018-06-14 12:52:53 -0700

    added dummy database for temp

--- server.js
+++ server.js
@@ -2,8 +2,32 @@ const express = require('express');
 
 const app = express();
 
+const database = {
+  users: [
+    {
+      id: 123,
+      name: 'luci',
+      email: 'lucifer@dogmail.com',
+      password: 'bullysticks',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: 831,
+      name: 'andrew',
+      email: 'druu831@gmail.com',
+      password: 's3cr3t',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+};
 app.get('/', (req, res) => {
   res.send('this is working');
+})
+
+app.post('/signin', (req, res) => {
+  res.json('signin!');
 })
 
 app.listen(3000, () => {
