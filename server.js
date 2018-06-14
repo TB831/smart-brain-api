28e466b16db88076236bba043851eee6e142356b
TB831 <druu831@gmail.com>
2018-06-14 13:31:47 -0700

    endpoint ./profile/:id now working

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 const database = {
   users: [
     {
-      id: 123,
+      id: '123',
       name: 'luci',
       email: 'lucifer@dogmail.com',
       password: 'bullysticks',
@@ -15,7 +15,7 @@ const database = {
       joined: new Date()
     },
     {
-      id: 831,
+      id: '831',
       name: 'andrew',
       email: 'druu831@gmail.com',
       password: 's3cr3t',
@@ -30,9 +30,9 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req, res) => {
   if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-    res.json("success!");
+    return res.json("success!");
   } else {
-    res.status(400).json('error logging in');
+    return res.status(400).json('error logging in');
   }
 })
 
@@ -47,6 +47,20 @@ app.post('/register', (req, res) => {
     joined: new Date()
   })
   res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+  const {id} = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  })
+  if (!found) {
+    res.status(400).json('ID not found')
+  }
 })
 
 app.listen(3000, () => {
