abdad418ce854b62de19981783549de41d3d2312
TB831 <druu831@gmail.com>
2018-06-15 14:54:09 -0700

    update Rank component to pass through props name/entries

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req, res) => {
   if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-    return res.json("success");
+    return res.json(database.users[0]);
   } else {
     return res.status(400).json('error logging in');
   }
