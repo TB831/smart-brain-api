895c38bd8fd5037ec660027ea565528ba5cd77bc
TB831 <druu831@gmail.com>
2018-06-16 16:33:36 -0700

    register now export to register.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
+
+const register = require('./controllers/register.js');
 
 const db = knex({
   client: 'pg',
@@ -18,10 +20,6 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
-// app.get('/', (req, res) => {
-//   res.send(database.users);
-// })
 
 app.post('/signin', (req, res) => {
   db.select('email', 'hash').from('login')
@@ -42,33 +40,7 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('wrong credentials'))
 })
 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt))
 
 
 app.get('/profile/:id', (req, res) => {
