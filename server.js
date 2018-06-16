b89429535d34ba572a701716ee3b1263d5b95c42
TB831 <druu831@gmail.com>
2018-06-15 17:13:48 -0700

    server retrieves profile endpoint by id successfully

--- server.js
+++ server.js
@@ -69,15 +69,16 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const {id} = req.params;
-  let found = false;
   db.select('*').from('users').where({
     id: id
   }).then(user => {
-    console.log(user)
+    if (user.length) {
+      res.json(user[0])
+    } else {
+      res.status(400).json('ID not found')
+    }
   })
-  if (!found) {
-    res.status(400).json('ID not found')
-  }
+  .catch(err => res.status(400).json('Error getting user'))
 })
 
 app.put('/image', (req, res) => {
