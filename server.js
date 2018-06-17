de5a37852ee23409ac60b29828ad1317352030f5
TB831 <druu831@gmail.com>
2018-06-16 18:48:16 -0700

    deploying DB to heroku

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image.js');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'Home',
-    password : '',
-    database : 'smartbrain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
