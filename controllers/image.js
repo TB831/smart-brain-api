917a35fd1875c7120141ad38da40485612d96e4a
TB831 <druu831@gmail.com>
2018-06-16 17:35:45 -0700

    backend now makes calls to clarifai api, and sends response to frontend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require('clarifai');
+const clarifai = new Clarifai.App({
+ apiKey: 'e1424d3a9deb43588f7ddfcacddb7e6f'
+});
+
+const handleApiCall = (req, res) => {
+  clarifai.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('API fail'))
+}
+
 const handleImage = (req, res, db) => {
   const {id} = req.body;
   db('users').where('id', '=', id)
@@ -10,5 +24,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-  handleImage: handleImage
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
 }
--- server.js
+++ server.js
@@ -26,12 +26,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/signin', (req, res) => signIn.handleSignIn(req, res, db, bcrypt));
-
 app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt));
-
 app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db));
-
 app.put('/image', (req, res) => image.handleImage(req, res, db));
+app.post('/imageurl', (req, res) => image.handleApiCall(req, res));
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
